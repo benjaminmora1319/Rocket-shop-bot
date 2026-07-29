eb56f54a1615c85d0fd9be47b1c6c6af8b5c8e9e
benjaminmora1319 <benjaminmora1319@gmail.com>
2026-07-29 00:28:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import requests
 import os
 from datetime import datetime
+
 OBJETIVO = {
     "nombre": "fennec",
     "color": "black",
     "calcomania": "ombre"
 }
+
 WEBHOOK = os.getenv("WEBHOOK_URL")
 URL_TIENDA = "https://api.tracker.gg/api/v2/rocket-league/standard/shop"
 CABECERAS = {"User-Agent": "Mozilla/5.0"}
+
 def revisar_tienda():
     if not WEBHOOK:
         print("Falta el secreto WEBHOOK_URL")
@@ -23,7 +26,7 @@ def revisar_tienda():
             c = str(item.get("paint","normal")).lower()
             d = str(item.get("decal","ninguna")).lower()
             if OBJETIVO["nombre"] in n and OBJETIVO["color"] in c and OBJETIVO["calcomania"] in d:
-                aviso = "🚨 ¡SALIÓ LO QUE BUSCABAS!"\n"
+                aviso = "¡LO ENCONTRAMOS!\n"
                 aviso += f"Objeto: {item.get('name')}\n"
                 aviso += f"Color: {item.get('paint','Normal')}\n"
                 aviso += f"Calcomanía: {item.get('decal','Ninguna')}"
